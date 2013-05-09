375cc1acaac298db6221e69c3d61ccf7dd470eeb
vedo <victor.diazosorio@gmail.com>
2013-05-09 17:10:33 -0400

    Beans de control

---
+++ src/controller/SocioBean.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javax.ejb.Remote;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Victor
+ * Date: 09-05-13
+ * Time: 05:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Remote
+public interface SocioBean {
+}
